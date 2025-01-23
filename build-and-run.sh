dbaf15c08afb7f2e5a2eb01ccfbe926b9a759f9f
Benjamin Flesch <bfitservice@gmail.com>
2025-01-23 11:26:16 +0100

    playing around

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ./scripts/build.sh && ./build-x86_64-linux/ofol/bin/ofol
+
+# meson setup --buildtype=release --prefix <prefix> build
+# meson compile -C build
+# DESTDIR="$(pwd)/lite-xl" meson install --skip-subprojects -C build
